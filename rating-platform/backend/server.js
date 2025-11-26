ea4ff51f01f18317b8dc4e4454b81d2ceb90007d
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2025-11-27 00:32:44 +0530

    Updated Backend

--- rating-platform/backend/server.js
+++ rating-platform/backend/server.js
@@ -4,6 +4,7 @@ import { testConnection } from './src/config/database.js';
 const PORT = process.env.PORT || 5000;
 
 
+
 testConnection().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
